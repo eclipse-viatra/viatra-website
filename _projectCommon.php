24a757e62c15b901925326b821f0a0e592c86821
dvarro <dvarro>
2012-08-09 05:13:28 +0000

    restructure left nav links

--- _projectCommon.php
+++ _projectCommon.php
@@ -22,6 +22,10 @@
 	$Nav->addCustomNav("Download", "/gmt/download/", "_self", 1);
 	$Nav->addCustomNav("Documentation", "/gmt/doc/", "_self", 1);
 	$Nav->addCustomNav("VIATRA2", "/gmt/VIATRA2/", "_self", 1);
+	$Nav->addCustomNav("About this project", "http://www.eclipse.org/projects/project.php?id=modeling.gmt.viatra2", "_self", 2);
 	$Nav->addCustomNav("Documentation", "/gmt/VIATRA2/doc/", "_self", 2);
 	$Nav->addCustomNav("Download", "/gmt/VIATRA2/download/", "_self", 2);
+	$Nav->addCustomNav("VIATRA2 Wiki", "http://wiki.eclipse.org/VIATRA2", "_blank", 2);
+	$Nav->addCustomNav("Partners and supporters", "http://wiki.eclipse.org/VIATRA2#Partners_and_supporters", "_blank", 2);
+	$Nav->addCustomNav("Contact", "http://www.eclipse.org/gmt/VIATRA2/contact.php", "_blank", 2);
 ?>
--- contact.php
+++ contact.php
@@ -22,11 +22,7 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
 	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-	$Nav->addCustomNav("VIATRA2 Wiki", "http://wiki.eclipse.org/VIATRA2", "_blank", 2);
-	$Nav->addCustomNav("Partners and supporters", "http://wiki.eclipse.org/VIATRA2#Partners_and_supporters", "_blank", 2);
-	$Nav->addCustomNav("Contact", "http://www.eclipse.org/gmt/VIATRA2/contact.php", "_blank", 2);	
-		
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);	
 	
 	# End: page-specific settings
 	#
