0aedd295acf50a8adfe2c6d0d51beecf429a532f
Abel Hegedus <abel.hegedus@mit.bme.hu>
2014-06-13 21:27:30 +0200

    moved news to subpage

---
+++ news.php
@@ -0,0 +1,30 @@
+<?php                                                             require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App  = new App();  $Nav  = new Nav();  $Menu   = new Menu();   include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+  #*****************************************************************************
+  #
+  # template.php
+  #
+  # Author:     Istvan Rath
+  # Date:     7-October-2008
+  #
+  # Description: 
+  #
+  #
+  #****************************************************************************
+  
+  #
+  # Begin: page-specific settings.  Change these. 
+  $pageTitle    = "VIATRA2 News";
+  $pageKeywords = "";
+  $pageAuthor   = "Istvan Rath";
+  #
+  
+  # Paste your HTML content between the EOHTML markers! 
+  $html = file_get_contents('pages/all_news.html');
+  $incubation = file_get_contents('pages/incubation.html');
+  
+  $html = str_replace("~~INCUBATION~~", $incubation, $html);
+
+  # Generate the web page
+  $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
