8514ed33415b04647dfb44aa105dd8c69d9b2747
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2014-10-18 21:13:21 +0200

    Updated website to use common buttons for navigation

--- _projectCommon.php
+++ _projectCommon.php
@@ -17,4 +17,52 @@
 
 	# To define additional CSS or other pre-body headers
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/viatra2/style.css"/>');
+	
+	// Initialize $variables.
+	$variables = array();
+	$links = array();
+	
+	$links[] = array(
+			'icon' => 'fa-download', // Required
+			'url' => '/viatra2/downloads.php', // Required
+			'title' => 'Download', // Required
+			//'target' => '_blank', // Optional
+			'text' => 'Update Sites, Additional Features' // Optional
+	);
+	
+#	$links[] = array(
+#			'icon' => 'fa-users', // Required
+#			'url' => '/incquery/getting_involved.php', // Required
+#			'title' => 'Getting Involved', // Required
+#			//'target' => '_blank', // Optional
+#			'text' => 'Git, Mailing List' // Optional
+#	);
+	
+	$links[] = array(
+			'icon' => 'fa-book', // Required
+			'url' => '/viatra2/documentation.php', // Required
+			'title' => 'Documentation', // Required
+			//'target' => '_blank', // Optional
+			'text' => 'Tutorials, Examples, Javadoc, Wiki' // Optional
+	);
+	
+	$links[] = array(
+			'icon' => 'fa-support', // Required
+			'url' => '/viatra2/contact.php', // Required
+			'title' => 'Support', // Required
+			//'target' => '_blank', // Optional
+			'text' => 'Bug Tracker, Forum' // Optional
+	);
+	
+	$variables['header_nav'] = array(
+			'links' =>  $links, // Required
+			'logo' => array( // Required
+					'src' => '/viatra2/images/viatra_logo.png', // Required
+					'alt' => 'The VIATRA project', // Optional
+					'url' => '/viatra2', // Optional
+					//'target' => '_blank' // Optional
+			),
+	);
+	// Set Solstice theme variables (Array)
+	$App->setThemeVariables($variables);
 ?>
